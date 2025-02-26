ea79581f467c99131c265b8cd7c6edf9724d6cc1
Hallvard Trætteberg <hallvard.traetteberg@gmail.com>
2025-02-26 23:14:25 +0100

    compute heading (compare compass)

--- halban-parent/sokoban-movements/src/main/java/no/hal/sokoban/movements/fx/PositionMovementController.java
+++ halban-parent/sokoban-movements/src/main/java/no/hal/sokoban/movements/fx/PositionMovementController.java
@@ -49,10 +49,10 @@ public class PositionMovementController implements SokobanGameSubController {
       } else {
         positionService.positionProperty().removeListener(positionListener);
         positionService.stop();
-        posText.setText("lat: -.-, long: -.-");
+        posText.setText(".lat,.long; d -> * | *");
       }
     });
-    posText = new Text("lat: -.-, long: -.-, dist: -.-");
+    posText = new Text(".lat,.long; d -> * | *");
     sensitivitySelector = new Slider(0, 10, 5);
     sensitivitySelector.setShowTickLabels(true);
     sensitivitySelector.setMajorTickUnit(5);
@@ -74,9 +74,10 @@ public class PositionMovementController implements SokobanGameSubController {
         startPosition = pos;
       }
       double distance = distance(startPosition, pos);
-      double heading = compassService != null ? compassService.getHeading() : -1;
-      this.posText.setText(String.format("pos: %.5f,%.5f; %.1f -> %.0f",
-          pos.getLatitude(), pos.getLongitude(), distance, heading));
+      double heading = heading(startPosition, pos);
+      double compass = compassService != null ? compassService.getHeading() : -2;
+      this.posText.setText("%.5f,%.5f; %.1f -> %.0f | %.0f".formatted(pos.getLatitude(), pos.getLongitude(),
+          distance, heading, compass));
     } catch (Exception e) {
       this.posText.setText(e.getMessage());
     }
@@ -86,18 +87,21 @@ public class PositionMovementController implements SokobanGameSubController {
     return distance(pos1.getLatitude(), pos2.getLatitude(), pos1.getLongitude(), pos2.getLongitude());
   }
 
+  public static double heading(Position pos1, Position pos2) {
+    return heading(pos1.getLatitude(), pos2.getLatitude(), pos1.getLongitude(), pos2.getLongitude());
+  }
+
   /**
    * From:
    * https://stackoverflow.com/questions/3694380/calculating-distance-between-two-points-using-latitude-longitude
-   * Calculate distance between two points in latitude and longitude taking
-   * into account height difference. If you are not interested in height
-   * difference pass 0.0. Uses Haversine method as its base.
+   * Calculate distance between two points in latitude and longitude taking.
+   * Uses Haversine method as its base.
    * 
    * @param lat1
    * @param lat2
    * @param lon1
    * @param lon2
-   * @return Distance in Meters
+   * @return Distance in meters
    */
   public static double distance(double lat1, double lat2, double lon1, double lon2) {
 
@@ -107,10 +111,30 @@ public class PositionMovementController implements SokobanGameSubController {
     double sinHalfLonDistance = Math.sin(Math.toRadians(lon2 - lon1) / 2);
     double a = sinHalfLatDistance * sinHalfLatDistance
         + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-            * sinHalfLonDistance * sinHalfLonDistance;
+          * sinHalfLonDistance * sinHalfLonDistance;
     double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     double distance = R * c * 1000; // convert to meters
 
-    return Math.sqrt(distance);
+    return distance;
+  }
+
+  /**
+   * From:
+   * http://www.movable-type.co.uk/scripts/latlong.html
+   * Calculate compass direction between two points in latitude and longitude.
+   * 
+   * @param lat1
+   * @param lat2
+   * @param lon1
+   * @param lon2
+   * @return Direction in degrees
+   */
+  public static double heading(double lat1, double lat2, double lon1, double lon2) {
+    double dLon = lon2 - lon1;
+    double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
+    double y = Math.sin(dLon) * Math.cos(lat2);
+    double heading = Math.toDegrees(Math.atan2(y, x));
+
+    return (heading + 360) % 360;
   }
 }
